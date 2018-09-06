d2238569943956b4a94f65076c376912a211ffd7
Shea Smith <51303984+SheaSmith@users.noreply.github.com>
2018-09-06 14:45:05 +1200

    More models, perhaps I should start studying...

---
+++ app/src/main/java/sheasmith/me/betterkamar/models/NZQAObject.java
@@ -0,0 +1,284 @@
+package sheasmith.me.betterkamar.models
+
+public class NZQAObjects
+{
+    private StudentOfficialResultsResults StudentOfficialResultsResults;
+
+    public StudentOfficialResultsResults getStudentOfficialResultsResults ()
+    {
+        return StudentOfficialResultsResults;
+    }
+
+    public void setStudentOfficialResultsResults (StudentOfficialResultsResults StudentOfficialResultsResults)
+    {
+        this.StudentOfficialResultsResults = StudentOfficialResultsResults;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [StudentOfficialResultsResults = "+StudentOfficialResultsResults+"]";
+    }
+
+    public class StudentOfficialResultsResults
+    {
+        private String AccessLevel;
+
+        private String NumberTypes;
+
+        private Types Types;
+
+        private String ErrorCode;
+
+        private String apiversion;
+
+        private String NumberRecords;
+
+        public String getAccessLevel ()
+        {
+            return AccessLevel;
+        }
+
+        public void setAccessLevel (String AccessLevel)
+        {
+            this.AccessLevel = AccessLevel;
+        }
+
+        public String getNumberTypes ()
+        {
+            return NumberTypes;
+        }
+
+        public void setNumberTypes (String NumberTypes)
+        {
+            this.NumberTypes = NumberTypes;
+        }
+
+        public Types getTypes ()
+        {
+            return Types;
+        }
+
+        public void setTypes (Types Types)
+        {
+            this.Types = Types;
+        }
+
+        public String getErrorCode ()
+        {
+            return ErrorCode;
+        }
+
+        public void setErrorCode (String ErrorCode)
+        {
+            this.ErrorCode = ErrorCode;
+        }
+
+        public String getApiversion ()
+        {
+            return apiversion;
+        }
+
+        public void setApiversion (String apiversion)
+        {
+            this.apiversion = apiversion;
+        }
+
+        public String getNumberRecords ()
+        {
+            return NumberRecords;
+        }
+
+        public void setNumberRecords (String NumberRecords)
+        {
+            this.NumberRecords = NumberRecords;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "ClassPojo [AccessLevel = "+AccessLevel+", NumberTypes = "+NumberTypes+", Types = "+Types+", ErrorCode = "+ErrorCode+", apiversion = "+apiversion+", NumberRecords = "+NumberRecords+"]";
+        }
+    }
+
+
+    public class Types
+    {
+        private Type[] Type;
+
+        public Type[] getType ()
+        {
+            return Type;
+        }
+
+        public void setType (Type[] Type)
+        {
+            this.Type = Type;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "ClassPojo [Type = "+Type+"]";
+        }
+    }
+
+    public class Qualification
+    {
+        private String Year;
+
+        private String Ref;
+
+        private String index;
+
+        private String Level;
+
+        private String Endorse;
+
+        private String Title;
+
+        public String getYear ()
+        {
+            return Year;
+        }
+
+        public void setYear (String Year)
+        {
+            this.Year = Year;
+        }
+
+        public String getRef ()
+        {
+            return Ref;
+        }
+
+        public void setRef (String Ref)
+        {
+            this.Ref = Ref;
+        }
+
+        public String getIndex ()
+        {
+            return index;
+        }
+
+        public void setIndex (String index)
+        {
+            this.index = index;
+        }
+
+        public String getLevel ()
+        {
+            return Level;
+        }
+
+        public void setLevel (String Level)
+        {
+            this.Level = Level;
+        }
+
+        public String getEndorse ()
+        {
+            return Endorse;
+        }
+
+        public void setEndorse (String Endorse)
+        {
+            this.Endorse = Endorse;
+        }
+
+        public String getTitle ()
+        {
+            return Title;
+        }
+
+        public void setTitle (String Title)
+        {
+            this.Title = Title;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "ClassPojo [Year = "+Year+", Ref = "+Ref+", index = "+index+", Level = "+Level+", Endorse = "+Endorse+", Title = "+Title+"]";
+        }
+    }
+
+    public class Type
+    {
+        private String index;
+
+        private String TypeCode;
+
+        private String NumberQuals;
+
+        private Qualifications Qualifications;
+
+        public String getIndex ()
+        {
+            return index;
+        }
+
+        public void setIndex (String index)
+        {
+            this.index = index;
+        }
+
+        public String getTypeCode ()
+        {
+            return TypeCode;
+        }
+
+        public void setTypeCode (String TypeCode)
+        {
+            this.TypeCode = TypeCode;
+        }
+
+        public String getNumberQuals ()
+        {
+            return NumberQuals;
+        }
+
+        public void setNumberQuals (String NumberQuals)
+        {
+            this.NumberQuals = NumberQuals;
+        }
+
+        public Qualifications getQualifications ()
+        {
+            return Qualifications;
+        }
+
+        public void setQualifications (Qualifications Qualifications)
+        {
+            this.Qualifications = Qualifications;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "ClassPojo [index = "+index+", TypeCode = "+TypeCode+", NumberQuals = "+NumberQuals+", Qualifications = "+Qualifications+"]";
+        }
+    }
+
+    public class Qualifications
+    {
+        private Qualification[] Qualification;
+
+        public Qualification[] getQualification ()
+        {
+            return Qualification;
+        }
+
+        public void setQualification (Qualification[] Qualification)
+        {
+            this.Qualification = Qualification;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "ClassPojo [Qualification = "+Qualification+"]";
+        }
+    }
+}
---
+++ app/src/main/java/sheasmith/me/betterkamar/models/ResultObject.java
@@ -0,0 +1,330 @@
+package sheasmith.me.betterkamar.models;
+
+public class ResultObject
+{
+    private StudentResultsResults StudentResultsResults;
+
+    public StudentResultsResults getStudentResultsResults ()
+    {
+        return StudentResultsResults;
+    }
+
+    public void setStudentResultsResults (StudentResultsResults StudentResultsResults)
+    {
+        this.StudentResultsResults = StudentResultsResults;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [StudentResultsResults = "+StudentResultsResults+"]";
+    }
+
+    public class StudentResultsResults
+    {
+        private String AccessLevel;
+
+        private String NumberResultLevels;
+
+        private String ErrorCode;
+
+        private ResultLevels ResultLevels;
+
+        private String apiversion;
+
+        private String NumberRecords;
+
+        private String StudentID;
+
+        public String getAccessLevel ()
+        {
+            return AccessLevel;
+        }
+
+        public void setAccessLevel (String AccessLevel)
+        {
+            this.AccessLevel = AccessLevel;
+        }
+
+        public String getNumberResultLevels ()
+        {
+            return NumberResultLevels;
+        }
+
+        public void setNumberResultLevels (String NumberResultLevels)
+        {
+            this.NumberResultLevels = NumberResultLevels;
+        }
+
+        public String getErrorCode ()
+        {
+            return ErrorCode;
+        }
+
+        public void setErrorCode (String ErrorCode)
+        {
+            this.ErrorCode = ErrorCode;
+        }
+
+        public ResultLevels getResultLevels ()
+        {
+            return ResultLevels;
+        }
+
+        public void setResultLevels (ResultLevels ResultLevels)
+        {
+            this.ResultLevels = ResultLevels;
+        }
+
+        public String getApiversion ()
+        {
+            return apiversion;
+        }
+
+        public void setApiversion (String apiversion)
+        {
+            this.apiversion = apiversion;
+        }
+
+        public String getNumberRecords ()
+        {
+            return NumberRecords;
+        }
+
+        public void setNumberRecords (String NumberRecords)
+        {
+            this.NumberRecords = NumberRecords;
+        }
+
+        public String getStudentID ()
+        {
+            return StudentID;
+        }
+
+        public void setStudentID (String StudentID)
+        {
+            this.StudentID = StudentID;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "ClassPojo [AccessLevel = "+AccessLevel+", NumberResultLevels = "+NumberResultLevels+", ErrorCode = "+ErrorCode+", ResultLevels = "+ResultLevels+", apiversion = "+apiversion+", NumberRecords = "+NumberRecords+", StudentID = "+StudentID+"]";
+        }
+    }
+
+    public class ResultLevels
+    {
+        private ResultLevel[] ResultLevel;
+
+        public ResultLevel[] getResultLevel ()
+        {
+            return ResultLevel;
+        }
+
+        public void setResultLevel (ResultLevel[] ResultLevel)
+        {
+            this.ResultLevel = ResultLevel;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "ClassPojo [ResultLevel = "+ResultLevel+"]";
+        }
+    }
+    public class Result
+    {
+        private String SubField;
+
+        private String CreditsPassed;
+
+        private String index;
+
+        private String Grade;
+
+        private String Credits;
+
+        private String Number;
+
+        private String ResultPublished;
+
+        private String Version;
+
+        private String Title;
+
+        public String getSubField ()
+        {
+            return SubField;
+        }
+
+        public void setSubField (String SubField)
+        {
+            this.SubField = SubField;
+        }
+
+        public String getCreditsPassed ()
+        {
+            return CreditsPassed;
+        }
+
+        public void setCreditsPassed (String CreditsPassed)
+        {
+            this.CreditsPassed = CreditsPassed;
+        }
+
+        public String getIndex ()
+        {
+            return index;
+        }
+
+        public void setIndex (String index)
+        {
+            this.index = index;
+        }
+
+        public String getGrade ()
+        {
+            return Grade;
+        }
+
+        public void setGrade (String Grade)
+        {
+            this.Grade = Grade;
+        }
+
+        public String getCredits ()
+        {
+            return Credits;
+        }
+
+        public void setCredits (String Credits)
+        {
+            this.Credits = Credits;
+        }
+
+        public String getNumber ()
+        {
+            return Number;
+        }
+
+        public void setNumber (String Number)
+        {
+            this.Number = Number;
+        }
+
+        public String getResultPublished ()
+        {
+            return ResultPublished;
+        }
+
+        public void setResultPublished (String ResultPublished)
+        {
+            this.ResultPublished = ResultPublished;
+        }
+
+        public String getVersion ()
+        {
+            return Version;
+        }
+
+        public void setVersion (String Version)
+        {
+            this.Version = Version;
+        }
+
+        public String getTitle ()
+        {
+            return Title;
+        }
+
+        public void setTitle (String Title)
+        {
+            this.Title = Title;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "ClassPojo [SubField = "+SubField+", CreditsPassed = "+CreditsPassed+", index = "+index+", Grade = "+Grade+", Credits = "+Credits+", Number = "+Number+", ResultPublished = "+ResultPublished+", Version = "+Version+", Title = "+Title+"]";
+        }
+    }
+
+    public class ResultLevel
+    {
+        private String NCEALevel;
+
+        private String index;
+
+        private String NumberResults;
+
+        private Results Results;
+
+        public String getNCEALevel ()
+        {
+            return NCEALevel;
+        }
+
+        public void setNCEALevel (String NCEALevel)
+        {
+            this.NCEALevel = NCEALevel;
+        }
+
+        public String getIndex ()
+        {
+            return index;
+        }
+
+        public void setIndex (String index)
+        {
+            this.index = index;
+        }
+
+        public String getNumberResults ()
+        {
+            return NumberResults;
+        }
+
+        public void setNumberResults (String NumberResults)
+        {
+            this.NumberResults = NumberResults;
+        }
+
+        public Results getResults ()
+        {
+            return Results;
+        }
+
+        public void setResults (Results Results)
+        {
+            this.Results = Results;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "ClassPojo [NCEALevel = "+NCEALevel+", index = "+index+", NumberResults = "+NumberResults+", Results = "+Results+"]";
+        }
+    }
+
+    public class Results
+    {
+        private Result Result;
+
+        public Result getResult ()
+        {
+            return Result;
+        }
+
+        public void setResult (Result Result)
+        {
+            this.Result = Result;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "ClassPojo [Result = "+Result+"]";
+        }
+    }
+}
